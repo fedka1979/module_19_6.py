faaa8215795f80f34c119b04bc5f0c1141851d2a
fedka1979 <fedka-3@yandex.ru>
2024-12-24 16:57:23 +0300

    Add files via upload

---
+++ myproject/myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
